88bf218e7d738431715973d67994e802206c99c2
MNaziran <20028828@myrp.edu.sg>
2022-07-08 16:30:41 +0800

    initial commit

---
+++ web/test2_fixed.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="This is test.php.">
+<title>Activity 4</title>
+</head>
+<body>
+    <h1>Page title</h1>
+    <h2>Section heading 1</h2>
+    <h3>Section Sub-heading</h3>
+</body>
+</html>
