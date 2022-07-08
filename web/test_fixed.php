dd613d4fe936bb9ddd05590f77c082cdda3f4647
MNaziran <20028828@myrp.edu.sg>
2022-07-08 14:38:08 +0800

    initial commit

--- web/test_fixed.php
+++ web/test_fixed.php
@@ -1,7 +1,6 @@
-<!-- fix : <html> element does not have a [lang] attribute -->
-<html lang="en">
-
+<html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
   Hello World!
