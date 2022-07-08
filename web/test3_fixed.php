60c299f19a8e8883e985c693efe279bd9b351b6a
MNaziran <20028828@myrp.edu.sg>
2022-07-08 21:54:13 +0800

    initial commit

---
+++ web/test3_fixed.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="This is test.php.">
+<title>Activity 4</title>
+</head>
+<body>
+    <p>Check out more of my <a href="portfolio.html">digital artwork</a>!</p>
+</body>
+</html>
