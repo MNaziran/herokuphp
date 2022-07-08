52acb6750427323808de4d8a8821501f57d3b2d0
MNaziran <20028828@myrp.edu.sg>
2022-07-08 15:08:54 +0800

    initial commit

--- web/test_fixed.php
+++ web/test_fixed.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="This is the ficed version of test.php.">
+<meta name="description" content="This is the fixed version of test.php.">
 <title>Activity 3</title>
 </head>
 <body>
