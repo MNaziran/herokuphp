9aebaea1354211f9fc990b352b71e882ac3b5810
MNaziran <20028828@myrp.edu.sg>
2022-07-08 16:00:37 +0800

    initial commit

---
+++ web/test1_fixed.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="This is test.php.">
+<title>Activity 4</title>
+</head>
+<body>
+  <img src=img/wallpaper-100w.jpg alt="wallpaper image">
+</body>
+</html>
